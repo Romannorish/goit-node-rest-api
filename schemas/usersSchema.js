edd7f29a4b2715841c243df431e8ae008628c9a7
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2024-05-26 21:42:07 +0300

    hw 4
    hw 4

--- schemas/usersSchema.js
+++ schemas/usersSchema.js
@@ -1,30 +1,48 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import Joi from "joi";
 
-const usersSchema = new mongoose.Schema(
-    {
-        password: {
-          type: String,
-          required: [true, "Password is required"],
-        },
-        email: {
-          type: String,
-          required: [true, "Email is required"],
-          unique: true,
-        },
-        subscription: {
-          type: String,
-          enum: ["starter", "pro", "business"],
-          default: "starter",
-        },
-        token: {
-          type: String,
-          default: null,
-        },
-      },
-      {
-        versionKey: false,
-        timestamps: true,
-      },
-)
+export const registerSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+  subscription: Joi.string().valid("starter", "pro", "business")
+})
 
-export const User = mongoose.model("User", usersSchema)
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required()
+})
+
+export const subscriptionSchema = Joi.object({
+  subscription: Joi.string().valid("starter", "pro", "business").required()
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+
+export const User = mongoose.model("User", userSchema)
